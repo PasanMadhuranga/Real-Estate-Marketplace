6d85c5fbe6d8b8b729484bd4ed5b634968de0d38
dimalsha <madushaniagd@gmail.com>
2024-06-12 22:46:19 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import {errorHandler} from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res,next) => {
     // console.log(req.body);
@@ -19,3 +20,25 @@ export const signup = async (req, res,next) => {
     }
     
 };
+
+export const signin = async (req,res,next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, "User not found"));
+
+        const validPassword = await bcryptjs.compare(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, "Invalid username or password"));
+        // If the credentials are valid, the server generates a JWT containing the user's ID and signs it with a secret key.
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        //The password is excluded from the user object to avoid sending it back to the client.
+        const {password:hashedPassword, ...restOfUser} = validUser._doc;
+        // The generated token is sent to the client in an HTTP-only cookie for security.
+        // The user data (excluding the password) is sent back in the response.
+        res.cookie('access_token', token, { httpOnly: true}).status(200).json(restOfUser);
+
+    } catch(error){
+        next(error);
+    }
+
+}    
