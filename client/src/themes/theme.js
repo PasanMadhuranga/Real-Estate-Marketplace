08fc3ab572512d2ad5327abe4228e1296f140314
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-15 14:13:01 +0530

    create theme for mui

---
+++ client/src/themes/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    nature: {
+      main: "#BFF6C3",
+      light: "#E0FBE2",
+      dark: "#dce6dc",
+      contrastText: "#ACE1AF",
+    },
+  },
+});
+
+
+export default theme;
