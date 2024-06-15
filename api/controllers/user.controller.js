095bacfbe8663da1a232bc85e5dc54aa037a7016
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-15 12:12:07 +0530

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import Listing from "../models/listing.model.js";
 
 export const updateUser = async (req, res, next) => {
   if(req.user.id !== req.params.id) return next(errorHandler(401, "You can update only your account" ));
@@ -49,6 +50,18 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token")
     res.status(200).json("User has been deleted");
+  } catch(error){
+    next(error);
+  }
+}
+
+
+export const getUserListings = async (req, res, next) => {
+  if(req.user.id !== req.params.id) return next(errorHandler(401, "You can get only your listings" ));
+  
+  try {
+    const listings = await Listing.find({userRef: req.params.id});
+    res.status(200).json(listings);
   } catch(error){
     next(error);
   }
