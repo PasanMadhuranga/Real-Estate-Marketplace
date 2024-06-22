e00511d9c58c99245418ade144e2443ca585354a
dimalsha <madushaniagd@gmail.com>
2024-06-22 15:34:33 +0530

    SetClientSide validations step 1

--- client/src/pages/EditListingWithoutValidation.jsx
+++ client/src/pages/EditListingWithoutValidation.jsx
@@ -9,7 +9,7 @@ import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
-import { useState ,useEffect , useRef} from "react";
+import { useState ,useEffect} from "react";
 import { app } from "../firebase";
 import {
   getStorage,
@@ -33,49 +33,20 @@ import ValidatedTextField from "../components/ValidatedTextField";
 export default function EditListing() {
   const [files, setFiles] = useState([]); //to choose the files
   const [loading, setLoading] = useState(false);
-  // const [formData, setFormData] = useState({
-  //   name: "",
-  //   description: "",
-  //   address: "",
-  //   type: "sell",
-  //   parking: false,
-  //   furnished: false,
-  //   offer: false,
-  //   bedrooms: 1,
-  //   bathrooms: 1,
-  //   regularPrice: 0,
-  //   discountPrice: 0,
-  //   imageUrls: [],
-  // });
-
-  const [nonValidatedformData, setNonValidatedformData] = useState({
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    address: "",
     type: "sell",
     parking: false,
     furnished: false,
     offer: false,
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 0,
+    discountPrice: 0,
     imageUrls: [],
   });
-
-  const [isFormValid,setIsFormValid] = useRef({
-    name: false,
-    description: false,
-    address: false,
-    bedrooms: false,
-    bathrooms: false,
-    regularPrice: false,
-    discountPrice: false,
-  });
-
-  const [validatedFormData,setValidatedFormData] = useState({
-    name: "",
-    description: "",  
-    address: "",
-    bedrooms: "",
-    bathrooms: "",
-    regularPrice: "",
-    discountPrice: "",
-  });
-
 
   const [imageUploadError, setImageUploadError] = useState("");
   const [uploading, setUploading] = useState(false);
@@ -92,7 +63,7 @@ export default function EditListing() {
     if (files.length === 0) {
       setImageUploadError("Please select an image");
     }
-    else if (files.length + nonValidatedformData.imageUrls.length <= 6) {
+    else if (files.length + formData.imageUrls.length <= 6) {
       setUploading(true);
       setImageUploadError("");
 
@@ -102,9 +73,9 @@ export default function EditListing() {
       }
       Promise.all(promises)
         .then((urls) => {
-          setNonValidatedformData({
-            ...nonValidatedformData,
-            imageUrls: nonValidatedformData.imageUrls.concat(urls),
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
           }
         );
           setImageUploadError("");
@@ -156,12 +127,12 @@ export default function EditListing() {
   //(_, i) is the callback function used by filter. The underscore _ represents the current element (which we don't need in this case), and i is the index of the current element.
   const handleDeleteImage = (index) => {
     const storage = getStorage(app);
-    const imageRef = ref(storage, nonValidatedformData.imageUrls[index]);
+    const imageRef = ref(storage, formData.imageUrls[index]);
     deleteObject(imageRef)
       .then(() => {
         console.log("Image deleted successfully");
-        const newImageUrls = nonValidatedformData.imageUrls.filter((_, i) => i !== index);
-        setFormData({ ...nonValidatedformData, imageUrls: newImageUrls });
+        const newImageUrls = formData.imageUrls.filter((_, i) => i !== index);
+        setFormData({ ...formData, imageUrls: newImageUrls });
       })
       .catch((error) => {
         setError("Failed to delete image");
@@ -182,13 +153,7 @@ export default function EditListing() {
         }
         // console.log("response.data", response.data);
         const {name, description, address, type, parking, furnished, offer, bedrooms, bathrooms, regularPrice, discountPrice, imageUrls} = response.data;
-        console.log("response.data", response.data);
-        // setFormData({name, description, address, type, parking, furnished, offer, bedrooms, bathrooms, regularPrice, discountPrice, imageUrls});
-        setNonValidatedformData({type, parking, furnished, offer, imageUrls});
-        // validatedFormData.current = {name, description, address, bedrooms, bathrooms, regularPrice, discountPrice};
-        setValidatedFormData({name, description, address, bedrooms, bathrooms, regularPrice, discountPrice});
-        console.log("nonValidatedformData", nonValidatedformData);
-        console.log("validatedFormData", validatedFormData);
+        setFormData({name, description, address, type, parking, furnished, offer, bedrooms, bathrooms, regularPrice, discountPrice, imageUrls});
       } catch (error) {}
     };
     fetchListing();
@@ -200,8 +165,7 @@ const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      // const body = {...formData};
-      const body = {...validatedFormData, ...nonValidatedformData};
+      const body = {...formData};
       if (body.imageUrls.length === 0) {
         setError("Please upload at least one image");
         setLoading(false);
@@ -210,12 +174,6 @@ const handleSubmit = async (e) => {
 
       if (Number(body.discountPrice) > Number(body.regularPrice)) {
         setError("Discount price cannot be greater than regular price");
-        setLoading(false);
-        return;
-      }
-
-      if (!Object.values(isFormValid.current).every((isValid) => isValid)) { // Check if all form fields are valid
-        setError("Invalid form data");
         setLoading(false);
         return;
       }
@@ -256,48 +214,60 @@ const handleSubmit = async (e) => {
       </Box>
       <Grid component="form" onSubmit={handleSubmit} container spacing={3}>
         <Grid item xs={12} md={6}>
-  
-          <ValidatedTextField
+          <TextField
+            name="name"
+            required
+            fullWidth
+            id="name"
             label="Name"
-            validator={ListingNameValidator}
-            onChangeFunc={(isValid, value) => {
-              isFormValid.name = isValid;
-              validatedFormData.name = value;
-            }}
-            sxProps={{ my: 1 }}
+            variant="outlined"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            sx={{ my: 1 }}
+            color="success"
           />
-          
-          <ValidatedTextField
+          <TextField
+            name="description"
+            required
+            fullWidth
+            id="description"
             label="Description"
-            validator={ListingDescriptionValidator}
-            onChangeFunc={(isValid, value) => {
-              isFormValid.description = isValid;
-              validatedFormData.description = value;
-            }}
-            sxProps={{ my: 1 }}
+            variant="outlined"
+            inputProps={{ minLength: 10 }}
             multiline
             rows={3}
+            value={formData.description}
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+            sx={{ my: 1 }}
+            color="success"
           />
-          
-          <ValidatedTextField
+          <TextField
+            name="address"
+            required
+            fullWidth
+            id="address"
             label="Address"
-            validator={ListingAddressValidator}
-            onChangeFunc={(isValid, value) => {
-              isFormValid.address = isValid;
-              validatedFormData.address = value;
-            }}
-            sxProps={{ my: 1 }}
+            autoFocus
+            variant="outlined"
             multiline
             rows={2}
+            value={formData.address}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
+            sx={{ my: 1 }}
+            color="success"
           />
           <Box sx={{ display: "flex", justifyContent: "space-between" }}>
             <RadioGroup
               row
               aria-labelledby="sell-rent"
               name="sell-rent-group"
-              value={nonValidatedformData.type}
+              value={formData.type}
               onChange={(e) =>
-                setNonValidatedformData({ ...nonValidatedformData, type: e.target.value })
+                setFormData({ ...formData, type: e.target.value })
               }
             >
               <FormControlLabel
@@ -319,9 +289,9 @@ const handleSubmit = async (e) => {
                     color="success"
                     name="ParkingSpot"
                     id="ParkingSpot"
-                    checked={nonValidatedformData.parking}
+                    checked={formData.parking}
                     onChange={(e) =>
-                      setNonValidatedformData({ ...nonValidatedformData, parking: e.target.checked })
+                      setFormData({ ...formData, parking: e.target.checked })
                     }
                   />
                 }
@@ -333,9 +303,9 @@ const handleSubmit = async (e) => {
                     color="success"
                     name="Furnished"
                     id="Furnished"
-                    checked={nonValidatedformData.furnished}
+                    checked={formData.furnished}
                     onChange={(e) =>
-                      setNonValidatedformData({ ...nonValidatedformData, furnished: e.target.checked })
+                      setFormData({ ...formData, furnished: e.target.checked })
                     }
                   />
                 }
@@ -348,7 +318,7 @@ const handleSubmit = async (e) => {
                     name="Offer"
                     id="Offer"
                     onChange={(e) =>
-                      setNonValidatedformData({ ...nonValidatedformData, offer: e.target.checked })
+                      setFormData({ ...formData, offer: e.target.checked })
                     }
                   />
                 }
@@ -357,60 +327,66 @@ const handleSubmit = async (e) => {
             </FormGroup>
           </Box>
           <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
-            
-            <ValidatedTextField
+            <TextField
               label="Bedrooms"
-              validator={ListingRoomValidator}
-              onChangeFunc={(isValid, value) => {
-                isFormValid.bedrooms = isValid;
-                validatedFormData.bedrooms = value;
-              }}
-              sxProps={{ my: 1 }}
               type="number"
+              variant="outlined"
+              name="bedrooms"
+              id="bedrooms"
+              sx={{ width: { xs: 200, sm: 300, md: 250 } }}
+              value={formData.bedrooms}
+              onChange={(e) =>
+                setFormData({ ...formData, bedrooms: e.target.value })
+              }
             />
-            
-            <ValidatedTextField
+            <TextField
               label="Bathrooms"
-              validator={ListingRoomValidator}
-              onChangeFunc={(isValid, value) => {
-                isFormValid.bathrooms = isValid;
-                validatedFormData.bathrooms = value;
-              }}
-              sxProps={{ my: 1 }}
               type="number"
+              variant="outlined"
+              // fullWidth
+              sx={{ width: { xs: 200, sm: 300, md: 250 } }}
+              name="bathrooms"
+              id="bathrooms"
+              value={formData.bathrooms}
+              onChange={(e) =>
+                setFormData({ ...formData, bathrooms: e.target.value })
+              }
             />
           </Box>
           <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
-            
-            <ValidatedTextField
+            <TextField
               label={
-                nonValidatedformData.type === "rent"
+                formData.type === "rent"
                   ? "Regular Price ($/Month)"
                   : "Regular Price ($)"
               }
-              validator={ListingPriceValidator}
-              onChangeFunc={(isValid, value) => {
-                isFormValid.regularPrice = isValid;
-                validatedFormData.regularPrice = value;
-              }}
-              sxProps={{ my: 1 }}
               type="number"
+              variant="outlined"
+              // fullWidth
+              sx={{ width: { xs: 200, sm: 300, md: 250 } }}
+              name="regularPrice"
+              id="regularPrice"
+              value={formData.regularPrice}
+              onChange={(e) =>
+                setFormData({ ...formData, regularPrice: e.target.value })
+              }
             />
 
-            
-            <ValidatedTextField
+            <TextField
               label={
-                nonValidatedformData.type === "rent"
+                formData.type === "rent"
                   ? "Discount Price ($/Month)"
                   : "Discount Price ($)"
               }
-              validator={ListingPriceValidator}
-              onChangeFunc={(isValid, value) => {
-                isFormValid.discountPrice = isValid;
-                validatedFormData.discountPrice = value;
-              }}
-              sxProps={{ my: 1 }}
               type="number"
+              variant="outlined"
+              sx={{ width: { xs: 200, sm: 300, md: 250 } }}
+              name="discountPrice"
+              id="discountPrice"
+              value={formData.discountPrice}
+              onChange={(e) =>
+                setFormData({ ...formData, discountPrice: e.target.value })
+              }
             />
           </Box>
           <Button
@@ -422,7 +398,7 @@ const handleSubmit = async (e) => {
             disabled={loading}
             color="success"
           >
-            {loading ? "Loading..." : "SAVE CHANGES"}
+            {loading ? "Loading..." : "EDIT LISTING"}
           </Button>
           {error && (
             <Alert sx={{ mb: 2 }} severity="error">
@@ -480,8 +456,8 @@ const handleSubmit = async (e) => {
             </Typography>
           )}
           <Box sx={{ display: "flex", flexWrap: "wrap", mt: 3 }}>
-            {nonValidatedformData.imageUrls.length > 0 &&
-              nonValidatedformData.imageUrls.map((url, index) => (
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, index) => (
                 <Card
                   key={url}
                   sx={{
